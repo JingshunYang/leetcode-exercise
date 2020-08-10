6913c566f45aed2a81646c5d1892e2ccb621abdb
杰森 <913185613@qq.com>
2020-08-10 14:04:39 +0800

    feat: 新增利口20题

---
+++ problem_set/20.有效的括号.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=java
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char pre_c = stack.pop();
+                if (pre_c != map.get(c)) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
+// @lc code=end
+
