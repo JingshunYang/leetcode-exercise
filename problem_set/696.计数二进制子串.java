ed5891318b91e3d257579e79168e1e5f32c1c835
杰森 <913185613@qq.com>
2020-08-12 15:29:56 +0800

    feat: added 696

---
+++ problem_set/696.计数二进制子串.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=696 lang=java
+ *
+ * [696] 计数二进制子串
+ */
+
+// @lc code=start
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int ptr = 0, n = s.length(), last = 0, ans = 0;
+        while (ptr < n) {
+            char c = s.charAt(ptr);
+            int count = 0;
+            while (ptr < n && s.charAt(ptr) == c) {
+                ++ptr;
+                ++count;
+            }
+            ans += Math.min(last, count);
+            last = count;
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
