fd97fddcc8093e64bfdee03537dc70051cbd4240
杰森 <913185613@qq.com>
2020-08-12 15:02:01 +0800

    feat: added 1441

---
+++ problem_set/1441.用栈操作构建数组.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1441 lang=java
+ *
+ * [1441] 用栈操作构建数组
+ */
+
+// @lc code=start
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+
+    }
+}
+// @lc code=end
+
