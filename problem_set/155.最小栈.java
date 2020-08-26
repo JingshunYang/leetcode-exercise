bd7d2b3f18af935e4bb87957eeef911ae2052cc7
杰森 <913185613@qq.com>
2020-08-26 17:06:49 +0800

    feat: added 155

--- problem_set/155.最小栈.java
+++ problem_set/155.最小栈.java
@@ -35,6 +35,50 @@ class MinStack {
 }
 
 /**
+ * 解法2：变量标记最小值
+ */
+
+class MinStack {
+
+    /** initialize your data structure here. */
+    Stack<Integer> s = new Stack<>();
+    int min = Integer.MAX_VALUE;
+
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+        if (min >= x) {
+            s.push(min);
+            min = x;
+        }
+        s.push(x);
+    }
+
+    public void pop() {
+        int temp = s.pop();
+        if (temp == min) {
+            min = s.pop();
+        }
+    }
+
+    public int top() {
+        return s.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such: MinStack obj =
+ * new MinStack(); obj.push(x); obj.pop(); int param_3 = obj.top(); int param_4
+ * = obj.getMin();
+ */
+
+/**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
  * obj.push(x);
